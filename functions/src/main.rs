ca60671c31dc178960d5d6581b003e3ff6913d3a
goodnews <goodnewsj62@gmail.com>
2024-02-27 09:33:37 +0100

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    another_function(56);
+    let x =  return_something();
+
+    println!("return_something returned {x}");
+}
+
+fn another_function(x:u32){
+    println!("hey i am {x} {}",  "another function");
+}
+
+fn return_something() -> i32{
+    66
+}
