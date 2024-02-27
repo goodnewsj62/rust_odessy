71ac280abe09c8c2515b410810752e6ee8786731
goodnews <goodnewsj62@gmail.com>
2024-02-27 09:54:47 +0100

    conditions

---
+++ branches/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let number =  3;
+
+    if number < 5{
+        println!("hey this number is less than 3");
+    }else{
+        println!("this number is greater than 5");
+    }
+
+
+    let inline_val =  if true {6} else {7};
+
+    println!("inline value: {inline_val}");
+}
