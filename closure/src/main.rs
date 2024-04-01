defbbb4109b9cfe6b2555c2d3377c0f8b897f3bc
goodnews <goodnewsj62@gmail.com>
2024-04-01 16:21:50 +0100

    closure knowledge

---
+++ closure/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let add_one_v3 =  |num|  num + 1;
+
+    let x = add_one_v3(1);
+
+    println!("printing the value of closure {}",  x);
+
+    let mut list =  vec![1,2,3,4];
+
+    println!("before defining closure {:?}",  list);
+
+    let mut borrow_mutably  =  || list.push(1);
+
+    borrow_mutably();
+
+    println!("after mutable borrow {:#?}",  list)
+}
